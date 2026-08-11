3dff255f4df820a10aa907b58878cf8466d0e540
NonFikShun <35634602+NonFikShun@users.noreply.github.com>
2026-08-11 08:54:07 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
-              <div className="flex items-center gap-2">
+              <div className="flex items-center gap-0">
               <img 
                 src="logo.png"
                alt="Brothers Pest Control Logo"
